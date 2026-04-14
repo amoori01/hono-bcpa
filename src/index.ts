a9c5b8ec7bf55438abe563a6c42a0159dfd9ad3e
amoori.01 <omarjafarcool@gmail.com>
2026-04-14 16:21:22 +0300

    Done with Assignment

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,78 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
 
-const app = new Hono()
+const app=new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get("/", (c)=>{
+  return c.text("Hello Hono!")
 })
 
-export default app
+type User= {
+id: string;
+name: string;
+email: string;
+password: string;
+}
+
+const users: User[]=[]
+app.get("/users",(c)=>{
+  return c.json(users)
+})
+
+app.get("/users/:id", (c) => {
+  const id = c.req.param("id"); // already string
+
+  const user = users.find((u) => u.id === id);
+
+  if (!user) {
+    return c.json({ error: "User not found" }, 404);
+  }
+
+  return c.json(user);
+});
+
+app.post("/signup", async (c) => {
+  const body = await c.req.json();
+  const { name, email, password } = body;
+
+  const exists = users.find((u) => u.email === email);
+  if (exists) {
+    return c.json({ error: "email already exists" }, 400);
+  }
+
+  const newUser: User = {
+    id: String(users.length + 1),
+    name,
+    email,
+    password,
+  };
+
+  users.push(newUser);
+
+  return c.json({
+    id: newUser.id,
+    name: newUser.name,
+    email: newUser.email,
+  });
+});
+
+app.post("/signin", async (c) => {
+  const body = await c.req.json();
+  const { email, password } = body;
+
+  const user = users.find((u) => u.email === email);
+
+  if (!user || user.password !== password) {
+    return c.json({ error: "invalid email or password" }, 401);
+  }
+
+  return c.json({
+    message: "login successful",
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    },
+  });
+});
+
+export default app;
